d0d4570285716a620bf2eb21477bfe20a7aa6be6
Regis Tangou <rtangou@gmail.com>
2022-12-07 21:19:33 +0000

    Start of my project

---
+++ Tech_E_Commerce/Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Tech_E_Commerce.Models;
+
+namespace Tech_E_Commerce
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Category> categories { get; set}
+    }
+}
---
+++ Tech_E_Commerce/Models/Category.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MVC_Reg_project.Models
+{
+    public class Category
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        public int displayOrder { get; set; }
+        public DateTime CreatedDate { get; set; } = DateTime.Now;
+
+    }
+}
